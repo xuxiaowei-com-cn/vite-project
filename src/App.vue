633e432187556210e3246310dbec3de644f4d4d5
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-12-06 14:37:28 +0800

    集成element-plus，自动导入

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,16 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<!--  <div>-->
+<!--    <a href="https://vitejs.dev" target="_blank">-->
+<!--      <img src="/vite.svg" class="logo" alt="Vite logo" />-->
+<!--    </a>-->
+<!--    <a href="https://vuejs.org/" target="_blank">-->
+<!--      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />-->
+<!--    </a>-->
+<!--  </div>-->
+<!--  <HelloWorld msg="Vite + Vue" />-->
+  <el-button>Default</el-button>
 </template>
 
 <style scoped>
